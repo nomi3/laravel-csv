ab98ae0546632dea299a2a678d0ac7dc1f72c177
nomi3 <thisisnomi3@gmail.com>
2024-04-25 22:44:08 +0900

    一覧画面に表示するデータの追加

--- src/resources/views/insureds/index.blade.php
+++ src/resources/views/insureds/index.blade.php
@@ -1,19 +1,38 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Sample Blade Page</title>
+    <title>保険証データ管理</title>
 </head>
 <body>
-    <h1>Welcome to the Sample Blade Page!</h1>
+    <h1>保険証データ一覧</h1>
 
-    <p>This is a sample Blade template.</p>
+    <p>登録されている保険証の情報を閲覧できます。</p>
 
     <a href="{{ route('insureds.create') }}">新規登録</a>
 
-    <ul>
-        @foreach ($insureds as $insured)
-            <li>{{ $insured->name }}</li>
-        @endforeach
-    </ul>
+    <table>
+        <thead>
+            <tr>
+                <th>漢字氏名</th>
+                <th>カナ（姓）</th>
+                <th>カナ（名）</th>
+                <th>メールアドレス</th>
+                <th>保険証番号</th>
+                <th>保険証記号</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($insureds as $insured)
+                <tr>
+                    <td>{{ $insured->name }}</td>
+                    <td>{{ $insured->last_name_kana }}</td>
+                    <td>{{ $insured->first_name_kana }}</td>
+                    <td>{{ $insured->email }}</td>
+                    <td>{{ $insured->insurance_card_number }}</td>
+                    <td>{{ $insured->insurance_card_symbol }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 </body>
 </html>
