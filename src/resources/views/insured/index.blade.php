e1d6980a4e49fc6d80594133e060eb36beb5498e
nomi3 <thisisnomi3@gmail.com>
2024-04-25 12:57:32 +0900

    被保険者用のリソース作成

---
+++ src/resources/views/insured/index.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Sample Blade Page</title>
+</head>
+<body>
+    <h1>Welcome to the Sample Blade Page!</h1>
+
+    <p>This is a sample Blade template.</p>
+</body>
+</html>
