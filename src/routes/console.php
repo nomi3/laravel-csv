a1c79127fa746899b276033586aab021d9aa9def
nomi3 <thisisnomi3@gmail.com>
2024-04-28 07:03:36 +0900

    スケジュールの設定

--- src/routes/console.php
+++ src/routes/console.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Usecases\Insured\SendEmails;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Artisan::command('insured:send-email', function (SendEmails $usecase) {
+    $usecase();
+})->purpose('Send email to insureds')->dailyAt('10:00');
